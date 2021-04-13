7b764bc33d2565de60a95a98feaad37480638d71
Tuegtgt11 <73330581+Tuegtgt11@users.noreply.github.com>
2021-04-13 21:23:01 +0700

    Task05

---
+++ Task5/CodeLeanE.java
@@ -0,0 +1,8 @@
+package com.codelean.until;
+import com.codelean.access.CodeLeanB;
+public class CodeLeanE extends CodeLeanB{
+    public static void main(String[] args){
+        new CodeLeanB().methodPublic();
+//        new CodeLeanB().methodProtected;
+    }
+}
