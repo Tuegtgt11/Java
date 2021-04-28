53979e14f4811daed36b880c1444194e4d6b7795
Tuegtgt11 <73330581+Tuegtgt11@users.noreply.github.com>
2021-04-28 14:34:06 +0700

    Practice

---
+++ Practice/Ex1/Bank.java
@@ -0,0 +1,40 @@
+package Practice.Ex1;
+
+public class Bank {
+    protected double balance;
+    protected double rate;
+    protected double interest;
+
+    public Bank() {}
+
+    public Bank(double balance, double rate) {
+        this.balance = balance;
+        this.rate = rate;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double calculateInterest () {
+        interest = balance * (rate / 1200);
+        return interest;
+    }
+
+    public String toString () {
+        return "Balance :" + balance + ", Rate :" + rate;
+    }
+
+}
